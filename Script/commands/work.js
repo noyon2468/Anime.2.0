dc4b5ec98b9683c4ac53f568b5e3edc230d13b27
noyon2468 <nh0743087@gmail.com>
2025-06-20 17:28:31 +0600

    Update work.js

--- Script/commands/work.js
+++ Script/commands/work.js
@@ -1,93 +1,65 @@
 module.exports.config = {
-	name: "work",
-	version: "1.0.1",
-	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
-	description: "Work to earn money!",
-	commandCategory: "Entertainment",
-    cooldowns: 5,
-    envConfig: {
-        cooldownTime: 1200000
-    }
+  name: "work",
+  version: "1.0.2",
+  hasPermssion: 0,
+  credits: "নূর মোহাম্মদ + ChatGPT",
+  description: "কাজ করে অর্থ উপার্জন করুন!",
+  commandCategory: "অর্থনীতি",
+  cooldowns: 5,
+  envConfig: {
+    cooldownTime: 1200000 // 20 মিনিট
+  }
 };
 
 module.exports.languages = {
-    "vi": {
-        "cooldown": "Bạn đã làm công việc hôm nay, để tránh kiệt sức hãy quay lại sau: %1 phút %2 giây.",
-        "rewarded": "Bạn đã làm công việc %1 và kiếm ra được %2$",
-        "job1": "bán vé số",
-        "job2": "sửa xe",
-        "job3": "lập trình",
-        "job4": "hack facebook",
-        "job5": "đầu bếp",
-        "job6": "thợ hồ",
-        "job7": "fake taxi",
-        "job8": "gangbang người nào đó",
-        "job9": "thợ sửa ống nước may mắn  ( ͡° ͜ʖ ͡°)",
-        "job10": "streamer",
-        "job11": "bán hàng trực tuyến",
-        "job12": "nội trợ",
-        "job13": 'bán "hoa"',
-        "job14": "tìm jav/hentai code cho SpermLord",
-        "job15": "chơi Yasuo và gánh đội của bạn"
-    },
-    "en": {
-        "cooldown": "You have worked today, to avoid exhaustion please come back after: %1 minute(s) %2 second(s).",
-        "rewarded": "You did the job: %1 and received: %2$.",
-        "job1": "Scammer",
-        "job2": "Mechanic",
-        "job3": "Programming",
-        "job4": "Hacker",
-        "job5": "Chef",
-        "job6": "Construction",
-        "job7": "Fake taxi",
-        "job8": "Gangbang someone",
-        "job9": "Plumber ( ͡° ͜ʖ ͡°)",
-        "job10": "Streamer",
-        "job11": "Online seller",
-        "job12": "Housewife",
-        "job13": 'Sell "flower"',
-        "job14": "find jav/hentai code for Scottz",
-        "job15": "Tiktoker"
-    }
-}
+  "bn": {
+    "cooldown": "আপনি ইতিমধ্যে আজ কাজ করেছেন। বিশ্রাম নিন এবং আবার চেষ্টা করুন: %1 মিনিট %2 সেকেন্ড পর।",
+    "rewarded": "✅ আপনি '%1' পেশায় কাজ করে আয় করেছেন 💸 %2 টাকা!",
+    "job1": "চা দোকানি",
+    "job2": "অ্যাপ ডেভেলপার",
+    "job3": "ইলেকট্রিশিয়ান",
+    "job4": "ফ্রিল্যান্সার",
+    "job5": "ব্লগার",
+    "job6": "রং মিস্ত্রি",
+    "job7": "ডেলিভারি বয়",
+    "job8": "কৃষক",
+    "job9": "পাঠ্যবই বিক্রেতা",
+    "job10": "ইমাম",
+    "job11": "মাঠকর্মী",
+    "job12": "দোকানি",
+    "job13": "চিত্রশিল্পী",
+    "job14": "শিক্ষক",
+    "job15": "ওয়েব ডিজাইনার"
+  }
+};
 
 module.exports.run = async ({ event, api, Currencies, getText }) => {
-    const { threadID, messageID, senderID } = event;
-    
-    const cooldown = global.configModule[this.config.name].cooldownTime;
-    let data = (await Currencies.getData(senderID)).data || {};
-    if (typeof data !== "undefined" && cooldown - (Date.now() - data.workTime) > 0) {
-        var time = cooldown - (Date.now() - data.workTime),
-            minutes = Math.floor(time / 60000),
-            seconds = ((time % 60000) / 1000).toFixed(0);
-        
-		return api.sendMessage(getText("cooldown", minutes, (seconds < 10 ? "0" + seconds : seconds)), event.threadID, event.messageID);
-    }
-    else {
-        const job = [
-            getText("job1"),
-            getText("job2"),
-            getText("job3"),
-            getText("job4"),
-            getText("job5"),
-            getText("job6"),
-            getText("job7"),
-            getText("job8"),
-            getText("job9"),
-            getText("job10"),
-            getText("job11"),
-            getText("job12"),
-            getText("job13"),
-            getText("job14"),
-            getText("job15")
-        ];
-        const amount = Math.floor(Math.random() * 900);
-        return api.sendMessage(getText("rewarded", job[Math.floor(Math.random() * job.length)], amount), threadID, async () => {
-            await Currencies.increaseMoney(senderID, parseInt(amount));
-            data.workTime = Date.now();
-            await Currencies.setData(event.senderID, { data });
-            return;
-        }, messageID);
-    }     
+  const { threadID, messageID, senderID } = event;
+  const cooldown = global.configModule[this.config.name].cooldownTime;
+
+  let data = (await Currencies.getData(senderID)).data || {};
+  if (typeof data !== "undefined" && cooldown - (Date.now() - data.workTime) > 0) {
+    var time = cooldown - (Date.now() - data.workTime),
+      minutes = Math.floor(time / 60000),
+      seconds = ((time % 60000) / 1000).toFixed(0);
+
+    return api.sendMessage(getText("cooldown", minutes, seconds.padStart(2, '0')), threadID, messageID);
+  } else {
+    const jobList = [
+      getText("job1"), getText("job2"), getText("job3"),
+      getText("job4"), getText("job5"), getText("job6"),
+      getText("job7"), getText("job8"), getText("job9"),
+      getText("job10"), getText("job11"), getText("job12"),
+      getText("job13"), getText("job14"), getText("job15")
+    ];
+
+    const selectedJob = jobList[Math.floor(Math.random() * jobList.length)];
+    const amountEarned = Math.floor(Math.random() * 800) + 100;
+
+    await Currencies.increaseMoney(senderID, amountEarned);
+    data.workTime = Date.now();
+    await Currencies.setData(senderID, { data });
+
+    return api.sendMessage(getText("rewarded", selectedJob, amountEarned), threadID, messageID);
   }
+};
