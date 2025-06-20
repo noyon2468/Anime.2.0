322f31e45e26f91b93b360802efc3a128350afa4
noyon2468 <nh0743087@gmail.com>
2025-06-20 19:33:40 +0600

    Update automention.js

--- Script/commands/automention.js
+++ Script/commands/automention.js
@@ -1,17 +1,22 @@
 module.exports.config = {
-	name: "automention",
-	version: "1.0.0",
-	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
-	description: "automent [mentioned]",
-	commandCategory: "other",
-	cooldowns: 5
+  name: "automention",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "CYBER BOT ⚠️ + Modified by নূর মোহাম্মদ",
+  description: "যাকে ট্যাগ করা হয়েছে, তাকে আবার রিপ্লাই ট্যাগ করা হবে",
+  commandCategory: "other",
+  cooldowns: 3
 };
 
-module.exports.run = function({ api, event }) {
-	if (Object.keys(event.mentions) == 0) return api.sendMessage(`@[${event.senderID}:0]`, event.threadID, event.messageID);
-	else {
-		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`${Object.values(event.mentions)[i].replace('@', '')}: @[${Object.keys(event.mentions)[i]}:0]`, event.threadID);
-		return;
-	}
-}
+module.exports.run = async ({ api, event }) => {
+  const { threadID, messageID, mentions, senderID } = event;
+
+  if (Object.keys(mentions).length === 0) {
+    return api.sendMessage(`📌 তুমি কাউকে ট্যাগ করোনি!\n\n👉 @[${senderID}:0] একা একা খেলো না!`, threadID, messageID);
+  } else {
+    for (let uid in mentions) {
+      const name = mentions[uid].replace('@', '');
+      api.sendMessage(`🔔 ${name} আপনি ট্যাগ হয়েছেন!\n👉 @[${uid}:0]`, threadID);
+    }
+  }
+};
