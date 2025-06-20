85cd9b93ce89b0703cac275bd70459b80645d6c2
noyon2468 <nh0743087@gmail.com>
2025-06-20 10:50:41 +0600

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -4,43 +4,37 @@ const fs = require("fs-extra");
 const moment = require("moment-timezone");
 
 module.exports.config = {
-    name: "admin",
-    version: "1.0.0",
-    hasPermssion: 0,
-    credits: "ULLASH", //don't change my credit 
-    description: "Show Owner Info",
-    commandCategory: "info",
-    usages: "",
-    cooldowns: 5
+  name: "admin",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "নূর মোহাম্মদ",
+  description: "Show Owner Info",
+  commandCategory: "info",
+  usages: "",
+  cooldowns: 5
 };
 
 module.exports.run = async function({ api, event }) {
-    var time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
+  const time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
 
-    var callback = () => api.sendMessage({
-        body: `
+  const callback = () => api.sendMessage({
+    body: `
 ┏━━━━━━━━━━━━━━━━━━━━━┓
-┃      🌟 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 🌟      
+┃     💠 𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 💠    
 ┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 👤 𝐍𝐚𝐦𝐞      : 𝐮 𝐥 𝐥 𝐚 𝐬 𝐡 ッ
-┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫    : 𝐌𝐚𝐥𝐞
-┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : 𝐈𝐧 𝐂𝐨𝐦𝐩𝐥𝐢𝐜𝐚𝐭𝐞𝐝
-┃ 🎂 𝐀𝐠𝐞       : 21
-┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : 𝐈𝐬𝐥𝐚𝐦
-┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : 𝐝𝐢𝐩𝐥𝐨𝐦𝐚 𝐢𝐧 𝐀𝐠𝐫𝐢𝐜𝐮𝐥𝐭𝐮𝐫𝐞
-┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : 𝐍𝐨𝐚𝐤𝐡𝐚𝐥𝐢, 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡
-┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 🎭 𝐓𝐢𝐤𝐭𝐨𝐤  : ullash01
-┃ 📢 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 : https://t.me/The_morning_star71
-┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : https://www.facebook.com/profile.php?id=100015168369582
-┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞:  ${time}
+┃ 👑 𝐍𝐚𝐦𝐞       : নূর মোহাম্মদ
+┃ 📍 𝐅𝐫𝐨𝐦       : বাংলাদেশ
+┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : ইসলাম ☪️
+┃ 💬 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞 : বাংলা, English
+┃ ❤️ 𝐒𝐭𝐚𝐭𝐮𝐬    : Alhamdulillah, I’m Fine
+┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤  : fb.com/nur.mohammad.367314
+┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝   : ${time}
 ┗━━━━━━━━━━━━━━━━━━━━━┛
-        `,
-        attachment: fs.createReadStream(__dirname + "/cache/1.png")
-    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
-  
-    return request(encodeURI(`https://graph.facebook.com/100000478146113/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
-        .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
-        .on('close', () => callback());
+    `,
+    attachment: fs.createReadStream(__dirname + "/cache/nur_profile.jpg")
+  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/nur_profile.jpg"));
+
+  return request(encodeURI(`https://graph.facebook.com/100035389598342/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
+    .pipe(fs.createWriteStream(__dirname + "/cache/nur_profile.jpg"))
+    .on("close", () => callback());
 };
