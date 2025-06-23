d31b6759e60c1b1286d55192833fdcbbe84452bf
noyon2468 <nh0743087@gmail.com>
2025-06-23 23:46:31 +0600

    Update clearcache.js

--- Script/commands/clearcache.js
+++ Script/commands/clearcache.js
@@ -13,7 +13,7 @@ module.exports.run = async function ({ event, api, Currencies, args, Threads }) 
 const { writeFileSync, readdirSync, existsSync, unlinkSync } = require('fs-extra');
 const permission = ["100035389598342"];
              if (!permission.includes(event.senderID))
-             return api.sendMessage("ULLASH only.", event.threadID, event.messageID);
+             return api.sendMessage("Nur Muhammad only.", event.threadID, event.messageID);
   /*
   if(args[0] == "spam"){
       const { resolve } = require('path');
